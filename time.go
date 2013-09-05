c25b79dc3ee81bd48c91b1c45a106b0d283d260a
傅小黑 <fuxiaohei@hexiaz.com>
2013-09-05 12:28:52 +0800

    add comments in time.go

--- time.go
+++ time.go
@@ -1,3 +1,17 @@
+// Copyright 2013 com authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"): you may
+// not use this file except in compliance with the License. You may obtain
+// a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
 package com
 
 import (
@@ -7,19 +21,33 @@ import (
 	"strconv"
 )
 
-// format unix time int to string
+// Format unix time int64 to string
 func Date(ti int64, format string) string {
 	t := time.Unix(int64(ti), 0)
 	return DateT(t, format)
 }
 
-// format unix time string to string
+// Format unix time string to string
 func DateS(ts string, format string) string {
 	i, _ := strconv.ParseInt(ts, 10, 64)
-	return DateT(i, format)
+	return Date(i, format)
 }
 
-// format time.Time to string
+// Format time.Time struct to string
+// MM - month - 01
+// M - month - 1, single bit
+// DD - day - 02
+// D - day 2
+// YYYY - year - 2006
+// YY - year - 06
+// HH - 24 hours - 03
+// H - 24 hours - 3
+// hh - 12 hours - 03
+// h - 12 hours - 3
+// mm - minute - 04
+// m - minute - 4
+// ss - second - 05
+// s - second = 5
 func DateT(t time.Time, format string) string {
 	res := strings.Replace(format, "MM", t.Format("01"), -1)
 	res = strings.Replace(res, "M", t.Format("1"), -1)
@@ -38,7 +66,7 @@ func DateT(t time.Time, format string) string {
 	return res
 }
 
-// get now unix timestamp int64
+// Get unix stamp int64 of now
 func Now() int64 {
 	return time.Now().Unix()
 }
