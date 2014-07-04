46f61fea74104dbd02d7ed9c96a667fba5b439de
Unknown <joe2010xtmf@163.com>
2014-07-03 21:25:41 -0400

    Add more convert functions

--- convert.go
+++ convert.go
@@ -26,6 +26,11 @@ func (f StrTo) Exist() bool {
 	return string(f) != string(0x1E)
 }
 
+func (f StrTo) Uint8() (uint8, error) {
+	v, err := strconv.ParseUint(f.String(), 10, 8)
+	return uint8(v), err
+}
+
 func (f StrTo) Int() (int, error) {
 	v, err := strconv.ParseInt(f.String(), 10, 32)
 	return int(v), err
@@ -34,6 +39,21 @@ func (f StrTo) Int() (int, error) {
 func (f StrTo) Int64() (int64, error) {
 	v, err := strconv.ParseInt(f.String(), 10, 64)
 	return int64(v), err
+}
+
+func (f StrTo) MustUint8() uint8 {
+	v, _ := f.Uint8()
+	return v
+}
+
+func (f StrTo) MustInt() int {
+	v, _ := f.Int()
+	return v
+}
+
+func (f StrTo) MustInt64() int64 {
+	v, _ := f.Int64()
+	return v
 }
 
 func (f StrTo) String() string {
