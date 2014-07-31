07b9dea0e3b245872e8ecc3ceb8edb71f58ac4b2
Unknwon <joe2010xtmf@163.com>
2014-07-31 14:42:13 -0400

    Add RandomCreateBytes

--- string.go
+++ string.go
@@ -16,13 +16,16 @@ package com
 
 import (
 	"crypto/md5"
+	"crypto/rand"
 	"crypto/sha1"
 	"crypto/sha256"
 	"fmt"
 	"hash"
 	"io"
+	r "math/rand"
 	"strconv"
 	"strings"
+	"time"
 	"unicode"
 )
 
@@ -119,4 +122,31 @@ func Reverse(s string) string {
 		runes[n] = rune
 	}
 	return string(runes[n:])
+}
+
+// RandomCreateBytes generate random []byte by specify chars.
+func RandomCreateBytes(n int, alphabets ...byte) []byte {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	var randby bool
+	if num, err := rand.Read(bytes); num != n || err != nil {
+		r.Seed(time.Now().UnixNano())
+		randby = true
+	}
+	for i, b := range bytes {
+		if len(alphabets) == 0 {
+			if randby {
+				bytes[i] = alphanum[r.Intn(len(alphanum))]
+			} else {
+				bytes[i] = alphanum[b%byte(len(alphanum))]
+			}
+		} else {
+			if randby {
+				bytes[i] = alphabets[r.Intn(len(alphabets))]
+			} else {
+				bytes[i] = alphabets[b%byte(len(alphabets))]
+			}
+		}
+	}
+	return bytes
 }
